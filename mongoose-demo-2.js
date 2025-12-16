698b1f275f6a159bc5b9a598bcc85d944e92ba58
jayedmonson <143557802+jayedmonson@users.noreply.github.com>
2025-12-15 18:07:34 -0600

    class completed

--- mongoose-demo-2.js
+++ mongoose-demo-2.js
@@ -10,4 +10,20 @@ db.once('open', function() {
   // we're connected!
 });
 
+const kittySchema = new mongoose.Schema ({
+  name: String
+});
 
+kittySchema.methods.speak = function () {
+  const greeting = this.name
+    ? "Meow name is " + this.name
+    : "I don't ahve a name";
+  console.log(greeting);
+}
+
+const Kitten = mongoose.model('Kitten', kittySchema);
+
+const fluffly = new Kitten({ name: 'fluffy' });
+
+const fluffy = new Kitten({ name: 'fluffy' });
+fluffly.speak();
